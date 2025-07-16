6117ac7dd18b7a12a555d2db1b9475927e91a614
Raveena112 <153164732+Raveena112@users.noreply.github.com>
2025-07-16 15:53:31 +0530

    Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Jenkins {
     public static void main(String[] args) {
        
-        System.out.println(Hello);
+        System.out.println("Hello");
     }
 }
